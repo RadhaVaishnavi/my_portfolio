1fff3919afbb472f831221ac9af036c81cb43194
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2026-02-19 19:30:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Project Carousel
+const projectSwiper = new Swiper('.project-swiper', {
+    loop: true,
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    breakpoints: { 640: { slidesPerView: 1 }, 1024: { slidesPerView: 2 } }
+});
+
+// Internship Carousel
+const internshipSwiper = new Swiper('.internship-swiper', {
+    loop: true,
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    breakpoints: { 640: { slidesPerView: 1 }, 1024: { slidesPerView: 2 } }
+});
+
+// Cert Carousel
+const certSwiper = new Swiper('.cert-swiper', {
+    loop: true,
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    breakpoints: { 640: { slidesPerView: 1 }, 1024: { slidesPerView: 3 } }
+});
